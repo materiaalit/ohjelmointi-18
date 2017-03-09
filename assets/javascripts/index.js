cd3d4b8d7f4acedf6194b06dcde1cc9ba92f3932
Kalle O Ilves <kalleilv@mba-13.cs.helsinki.fi>
2017-03-09 13:45:37 +0200

    ohja config to student dashboard

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -4,6 +4,8 @@ try {
   console.log(e);
 }
 
+import * as store from 'store';
+
 import '../stylesheets/index.scss';
 
 import Exercises from './exercises';
@@ -11,7 +13,17 @@ import TableOfContents from './table-of-contents';
 import LoginModal from './login-modal';
 import Navigation from './navigation';
 
+function loginReset() {
+  if (!store.get('tmc.loginReset')) {
+    store.remove('tmc.user');
+    store.remove('tmc.course');
+    store.set('tmc.loginReset', '1');
+  }
+}
+
 $(() => {
+  loginReset();
+
   (new Exercises()).mount();
   (new TableOfContents()).mount();
   (new LoginModal()).mount();
