cb55aba6c519153b8bf8ba0b5a67510a8b7aa017
avihavai <avihavai@hp8x-64.cs.helsinki.fi>
2017-09-12 01:33:03 +0300

    p2 release

--- assets/javascripts/student-dashboard.js
+++ assets/javascripts/student-dashboard.js
@@ -13,6 +13,7 @@ function getOhpeSyksy17Config() {
     courseName: 'Ohjelmoinnin perusteet',
     exerciseGroups: {
       'Osa 1': ['05.09.2017 16:00', '11.09.2017 23:59', 'osa01-']
+      'Osa 2': ['12.09.2017 16:00', '18.09.2017 23:59', 'osa02-']
     },
   }
 }
